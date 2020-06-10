ca05301e6d069165ccadfd89197838737a80ba94
Universecitiz3n <maciej.horbacz@gmail.com>
2020-06-10 20:16:55 +0200

    Ow wrong file...

--- Configuration/Invoke-Test.ps1
+++ Configuration/Invoke-Test.ps1
@@ -1,65 +1,70 @@
 #params
-params(
-[String]$PackagePath
+param(
+    [String]$PackagePath
 )
+
 $SandboxOperatingFolder = 'C:\SandboxEnvironment' 
 $SandboxFile = "$((get-item $PackagePath).BaseName).wsb"
-$FolderPath = Split-Path (Split-Path "$ScriptPath" -Parent) -Leaf
-$FileName = (get-item $ScriptPath).Name
+$FolderPath = Split-Path (Split-Path "$PackagePath" -Parent) -Leaf
+$FileName = (get-item $PackagePath).Name
 
 $SandboxDesktopPath = "C:\Users\WDAGUtilityAccount\Desktop"
+$SandboxTempFolder = 'C:\Temp' 
 $SandboxSharedPath = "$SandboxDesktopPath\$FolderPath"
 $FullStartupPath = "$SandboxSharedPath\$FileName"
 $FullStartupPath = """$FullStartupPath"""
 
 If (!(Test-Path -Path $SandboxOperatingFolder -PathType Container))
 {
-New-Item -Path $SandboxOperatingFolder -ItemType Directory
+    New-Item -Path $SandboxOperatingFolder -ItemType Directory
 }
 Function New-WSB
 {
-Param
-(
-    [String]$CommandtoRun	
-)	
+    Param
+    (
+        [String]$CommandtoRun	
+    )	
         
-new-item -Path $SandboxOperatingFolder -Name $SandboxFile -type file -force | out-null
-$Config = @"
-"<Configuration>"
-"<VGpu>Enable</VGpu>"
-"<Networking>Enable</Networking>"
-"<MappedFolders>"	
-"<MappedFolder>"	
-"<HostFolder>$((get-item $PackagePath).Directory)</HostFolder>"	
-"<ReadOnly>true</ReadOnly>"	
-"</MappedFolder>"
-"<MappedFolder>"	
-"<HostFolder>C:\SandboxEnvironment</HostFolder>"	
-"<ReadOnly>true</ReadOnly>"	
-"</MappedFolder>"	
-"</MappedFolders>"	
-"<LogonCommand>"	
-"<Command>$CommandtoRun</Command>"	
-"</LogonCommand>"	
-"</Configuration>"
+    new-item -Path $SandboxOperatingFolder -Name $SandboxFile -type file -force | out-null
+    $Config = @"
+<Configuration>
+<VGpu>Enable</VGpu>
+<Networking>Enable</Networking>
+<MappedFolders>	
+<MappedFolder>	
+<HostFolder>$((get-item $PackagePath).Directory)</HostFolder>	
+<ReadOnly>true</ReadOnly>	
+</MappedFolder>
+<MappedFolder>	
+<HostFolder>C:\SandboxEnvironment\bin</HostFolder>	
+<ReadOnly>true</ReadOnly>	
+</MappedFolder>	
+</MappedFolders>	
+<LogonCommand>	
+<Command>$CommandtoRun</Command>	
+</LogonCommand>	
+</Configuration>
 "@
-Set-Content -Path $SandboxOperatingFolder -Name $SandboxFile -Value $Config
+    Set-Content -Path "$SandboxOperatingFolder\$SandboxFile" -Value $Config
 }
 
 
-$ScriptBlock = {
-$OperatingFolder = 'C:\Temp'
-$FullStartupPath
-
-If (!(Test-Path -Path $OperatingFolder -PathType Container))
+$ScriptBlock = @"
+If (!(Test-Path -Path $SandboxTempFolder -PathType Container))
 {
-    New-Item -Path $OperatingFolder -ItemType Directory
+    New-Item -Path $SandboxTempFolder -ItemType Directory
 }
-Copy-Item -Path $FullStartupPath -Destination $OperatingFolder
-Start-Process -FilePath $SandboxDesktopPath\SandboxEnvironment\IntuneWinAppUtilDecoder.exe -ArgumentList "$OperatingFolder\$SandboxFile"
-}
-$Script:Startup_Command = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -sta -WindowStyle Hidden -noprofile -executionpolicy unrestricted -Command $ScriptBlock"			
+Copy-Item -Path $FullStartupPath -Destination $SandboxTempFolder
+Start-Process -FilePath $SandboxDesktopPath\bin\IntuneWinAppUtilDecoder.exe -ArgumentList "$SandboxTempFolder\$FileName /s"
+Start-Sleep 2
+Move-Item -Path "$SandboxTempFolder\$FileName.decoded" -Destination "$SandboxTempFolder\$($FileName -replace '.intunewin','.zip')" -Force
+Expand-Archive -Path "$SandboxTempFolder\$($FileName -replace '.intunewin','.zip')" -Destination $SandboxTempFolder -Force
+Remove-Item -Path "$SandboxTempFolder\$($FileName -replace '.intunewin','.zip')" -Force
+Start-Process -FilePath $SandboxDesktopPath\bin\PsExec64.exe -ArgumentList "-accepteula -s '$SandboxTempFolder\$($FileName -replace '.intunewin','.ps1')'"
+"@
+New-Item -Path $SandboxOperatingFolder\bin -Name LogonCommand.ps1 -ItemType File -Value $ScriptBlock -Force | Out-Null
+$Script:Startup_Command = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -sta -WindowStyle Hidden -noprofile -executionpolicy unrestricted -File $SandboxDesktopPath\bin\LogonCommand.ps1"			
 
 New-WSB -CommandtoRun $Startup_Command		
 
-Start-Process $SandboxFile
+Start-Process $SandboxOperatingFolder\$SandboxFile
